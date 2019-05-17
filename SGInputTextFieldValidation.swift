20096e74b0dff6c6bd612df9b3689414836349ee
SuneelGunupudi <suneel.g@pixuate.com>
2019-05-17 13:25:09 +0530

    Add the Code

---
+++ SGInputTextFieldValidation.swift
@@ -0,0 +1,34 @@
+
+let inverseSet = NSCharacterSet(charactersIn:"0123456789").inverted
+let components = string.components(separatedBy: inverseSet)
+let filtered = components.joined(separator: "")
+if filtered == string {
+    return true
+} else {
+    if string == "." {
+        guard let text = textField.text else { return true }
+        let countdots = textField.text!.components(separatedBy:".").count - 1
+        if countdots == 0 {
+            
+            if text.isEmpty {
+                let newText = "0" + string
+                textField.text = newText
+                return false
+            }
+            return true
+        }else{
+            if countdots > 0 && string == "." {
+                return false
+            } else {
+                if text.isEmpty {
+                    let newText = "0" + string
+                    textField.text = newText
+                    return false
+                }
+                return true
+            }
+        }
+    }else{
+        return false
+    }
+}
